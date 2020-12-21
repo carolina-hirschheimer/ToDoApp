360b5fe51eb01a1cc5f635a6befa9e185fcdba84
carolina-hirschheimer <carolinah@al.insper.edu.br>
2020-12-21 15:56:40 -0300

    Backend project initialized

--- App/todoapp/lib/models/global.dart
+++ App/todoapp/lib/models/global.dart
@@ -3,4 +3,17 @@ import 'package:flutter/material.dart';
 Color darkGreyColor = new Color(0xFF212128);
 Color greenColor = new Color(0xFF81E6DF);
 
-TextStyle intrayTitleStyle = new TextStyle(fontFamily: 'Avenir', fontWeight: FontWeight.bold, color: darkGreyColor, fontSize:40);
+TextStyle intrayTitleStyle = new TextStyle(
+  fontFamily: 'Avenir', 
+  fontWeight: FontWeight.bold, 
+  color: darkGreyColor, 
+  fontSize:40
+  );
+
+TextStyle darkTodoTitle = new TextStyle(
+  fontFamily: 'Avenir', 
+  fontWeight: FontWeight.bold, 
+  color: darkGreyColor, 
+  fontSize:30
+  );
+
